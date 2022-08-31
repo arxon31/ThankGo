dcd8faf2ae14dce0aadbe7268e4d38528d05c35d
Elizar <arxon31@gmail.com>
2022-08-31 09:53:42 +0300

    Solved problem from 1.3

---
+++ 1.Base/1.3.1_Shortener/cut.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var text string
+	var width int
+	var res string
+	fmt.Scanf("%s %d", &text, &width)
+	if len(text) > width {
+		res = text[:width] + "..."
+	} else {
+		res = text
+	}
+	fmt.Println(res)
+}
