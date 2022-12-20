7d804c0bd733a3ba16aacf9d954d0ef67c4de8e6
Elizar <arxon31@gmail.com>
2022-12-20 16:21:49 +0300

    Solved problem with map

---
+++ 1.Base/1.3.2_Map/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func main() {
+	var number int
+	fmt.Scanf("%d", &number)
+
+	// Посчитайте, сколько раз каждая цифра встречается
+	// в числе `number`. Запишите результат в карту `counter`,
+	// где ключом является цифра числа, а значением -
+	// сколько раз она встречается
+	counter := make(map[int]int)
+	for {
+		i := 10
+		val := number % i
+		_, ok := counter[val]
+		if ok == false {
+			counter[val] = 1
+		} else {
+			counter[val]++
+		}
+		number = number / i
+		if number == 0 {
+			break
+		}
+	}
+
+	printCounter(counter)
+}
+
+// ┌─────────────────────────────────┐
+// │ не меняйте код ниже этой строки │
+// └─────────────────────────────────┘
+
+// printCounter печатает карту в формате
+// key1:val1 key2:val2 ... keyN:valN
+func printCounter(counter map[int]int) {
+	digits := make([]int, 0)
+	for d := range counter {
+		digits = append(digits, d)
+	}
+	sort.Ints(digits)
+	for idx, digit := range digits {
+		fmt.Printf("%d:%d", digit, counter[digit])
+		if idx < len(digits)-1 {
+			fmt.Print(" ")
+		}
+	}
+	fmt.Print("\n")
+}
